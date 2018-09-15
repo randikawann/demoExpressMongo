53e5a173fe8eccb37f84c09cbe203f107720677b
randikawann <randikawann@gmail.com>
2018-09-15 14:50:36 -0500

    added put request with update

--- routes/api.js
+++ routes/api.js
@@ -15,14 +15,17 @@ router.post('/ninja',function(req,res,next){
 });
 
 router.put('/ninja/:id',function(req,res,next){
-    res.send({type : 'PUT'});
+    // res.send({type : 'PUT'});
+    Student.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(student){
+        res.send(student);
+    });
 });
 
 router.delete('/ninja/:id',function(req,res,next){
     // console.log(req.params.id);
     // res.send({type : 'DELETE'});
     Student.findByIdAndRemove({_id: req.params.id}).then(function(student){
-        res.send("removed"+student);
+        res.send("removed "+student);
     });
 });
 
